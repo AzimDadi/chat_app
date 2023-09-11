25535673a54eaf817648b95ab84e489d3f5b8b12
Azim Dadi <122913134+AzimDadi@users.noreply.github.com>
2023-09-11 09:08:13 +0530

    Update firebase.js
    another commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-hide
+const firebaseConfig = 
+  const firebaseConfig = {
+  apiKey: "AIzaSyDVi2BYQt96hzF7sZYcMbpAGPIlTxjavVc",
+  authDomain: "chat--app2-92557.firebaseapp.com",
+  projectId: "chat--app2-92557",
+  storageBucket: "chat--app2-92557.appspot.com",
+  messagingSenderId: "787311411135",
+  appId: "1:787311411135:web:60e414eee24a86149f2b0a"
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
